58591552923c1d350c4c24ae89a0dedc690ecf8e
Deepika <deepika.v3298@gmail.com>
2023-07-26 21:28:45 +0530

    Initial author routes

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-//  if(process.env.NODE_ENV!=='production'){
-//     require('dotenv').config();
-//  }
- 
  const express= require('express');
  const app= express();
  const expressLayouts= require('express-ejs-layouts')
+ const bodyparser= require('body-parser')
 
  const indexRouters= require('./routes/index')
+ const authorRouters= require('./routes/authors')
 
  app.set('view engine', 'ejs')
  app.set('views', __dirname + '/views')
  app.set('layout','./layouts/layout')
  app.use(expressLayouts)
  app.use(express.static('public'))
+ app.use(bodyparser.urlencoded({limit:'10mb', extended:false}))
 
  const mongoose= require('mongoose')
  mongoose.connect("mongodb://127.0.0.1:27017/mybrary",{useNewUrlParser:true},{useUnifiedTopology:true}).then(()=>{
@@ -20,10 +19,8 @@
 }).catch((err)=>{
    console.log(err)
 })
-//  const db=mongoose.connection
-//  db.on('error',error=>console.error(error))
-//  db.once('open',()=>console.log('CONNECTED TO MONGOOSE'))
 
  app.use('/',indexRouters)
+ app.use('/authors',authorRouters)
 
  app.listen(process.env.PORT || 3000)
